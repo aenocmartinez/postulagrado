0daf8baf2b22ed6edc20d537ba4d248ae616dac5
Abimelec Martinez <aenoc.martinez@gmail.com>
2025-03-09 06:26:59 -0500

    :recycle: Implementación Inyección de Dependencia Seguimiento

--- app/Http/Controllers/SeguimientoController.php
+++ app/Http/Controllers/SeguimientoController.php
@@ -5,20 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Src\admisiones\usecase\procesos\BuscarProcesoUseCase;
 use Src\admisiones\usecase\procesos\ListarProcesosUseCase;
+use Src\shared\di\FabricaDeRepositorios;
 
 class SeguimientoController extends Controller
 {
     public function index()
     {
-        $procesos = ListarProcesosUseCase::ejecutar();
+        $listaProcesos = new ListarProcesosUseCase(
+            FabricaDeRepositorios::getInstance()->getProcesoRepository()
+        );
+
+        $response = $listaProcesos->ejecutar();
         return view('seguimientos.index', [
-            'procesos' => $procesos
+            'procesos' => $response->getData()
         ]);
     }
 
     public function show($procesoID)
     {
-        $response = BuscarProcesoUseCase::ejecutar($procesoID);
+        $buscarProceso = new BuscarProcesoUseCase(
+            FabricaDeRepositorios::getInstance()->getProcesoRepository()
+        );
+
+        $response = $buscarProceso->ejecutar($procesoID);
         if ($response->getCode() != 200) {
             return redirect()->route('seguimientos.index')->with($response->getCode(), $response->getMessage());
         }
